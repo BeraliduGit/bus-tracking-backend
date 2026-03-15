76a878ace2a641e787fd912dbbb4d5875848aa7d
Wickramasingha M.P.D.N. <130473106+Dilshan-Nuwantha@users.noreply.github.com>
2026-03-15 20:57:43 +0530

    edited

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,15 @@
 import mongoose from "mongoose";
 const connectToDatabase = async () => {
+    const mongoUri = process.env.MONGODB_URI || process.env.MONGO_URI;
+
     try {
-        connectToDatabase = await mongoose.connect(process.env.MONGODB_URI);
+        const connection = await mongoose.connect(mongoUri);
         console.log("\n Connected to MongoDB");
-        console.log(`Connected to MongoDB at ${connectToDatabase.connection.host}`);
+        console.log(`Connected to MongoDB at ${connection.connection.host}`);
 
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
-        process.exit(1); // Exit the process with an error code
+        process.exit(1); 
 
     }
 }
